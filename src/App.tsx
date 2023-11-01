ce2499ee8152196f1e7ce7d561482e27e88b2414
Andrey Pestunov <masterlifting@gmail.com>
2023-11-01 14:39:34 +0100

    feat: add template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { Counter } from './domains/counter/Counter';
+
+export default function App() {
+  return (
+    <>
+      <Counter />
+      <Counter />
+      <Counter />
+    </>
+  );
+}
---
+++ src/domains/counter/Counter.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
