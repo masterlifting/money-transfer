d76d0cdcd344ed7d2b05ec94f283fca311ca7a0f
Andrey Pestunov <masterlifting@gmail.com>
2023-12-13 11:42:04 +0100

    feat: configure ts

--- backend/src/controllers/usersController.ts
+++ backend/src/controllers/usersController.ts
@@ -1,9 +1,17 @@
 /** @format */
 
+import { IUserGet } from './../../../shared/types/UserTypes';
+
 import { Request, Response } from 'express';
 
+const _users: IUserGet[] = [];
+
 export const getUsers = (req: Request, res: Response) => {
-  res.send('Hello from the getUsers controller!');
+  _users.push({
+    id: '1',
+    email: 'test',
+  });
+  res.send(_users);
 };
 
 export const getUserBalance = (req: Request, res: Response) => {
